d8147817b788bebddff5a98ee5115b7683486552
MrFrangipane <contact@frangipane.me>
2025-10-14 21:02:30 +0200

    feat(Scripts): Melinerion

---
+++ scripts/melinerion.py
@@ -0,0 +1,70 @@
+import json
+import math
+
+from ledboardlib import BoardApi, ColorFormat, GpioEnum, SamplingPoint
+
+
+def compute_distances(points_):
+    distances_ = []
+    for i in range(len(points_) - 1):
+        x0, y0 = points_[i]
+        x1, y1 = points_[i + 1]
+        distances_.append(math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2))
+    return distances_
+
+
+if __name__ == "__main__":
+    port = "COM17"
+    scanned_points_filepath = "detec-melinerion-14-10-2025.json"
+
+    points = []
+    with open(scanned_points_filepath, "r") as file:
+        points = json.load(file)
+
+    distances = compute_distances(points)
+    average_distance = sum(distances) / len(distances)
+    print(f"Average distance: {average_distance}, for {len(distances)} segments")
+
+    filtered_distances = []
+    for distance in distances:
+        if distance <= average_distance * 3:
+            filtered_distances.append(distance)
+
+    average_filtered_distance = sum(filtered_distances) / len(filtered_distances)
+    print(f"Average filtered distance: {average_filtered_distance}, for {len(filtered_distances)} segments")
+
+    scaled_points = [(x / average_filtered_distance, y / average_filtered_distance) for x, y in points]
+    scaled_distances = compute_distances(scaled_points)
+    average_distance_scaled = sum(scaled_distances) / len(scaled_distances)
+
+    strand_led_count = 600
+
+    strand_count = 1
+    pixel_doubling = 1
+
+    board = BoardApi(serial_port_name=port)
+    configuration = board.get_configuration()
+    configuration.name = "Meliner"
+    configuration.led_color_format = ColorFormat.GRBW
+    configuration.led_count = strand_led_count
+    configuration.gpio_led_first = GpioEnum.LedsNoonBoard.value
+    configuration.gpio_dmx_input = GpioEnum.DmxNoonBoard.value
+    board.set_configuration(configuration)
+
+    print(configuration)
+
+    sampling_points = list()
+    for index, (x, y) in enumerate(scaled_points):
+        new = SamplingPoint(
+            index=index,
+            x=int(x),
+            y=int(y),
+            universe_number=0,
+            universe_channel=index * 3,
+            color_format=ColorFormat.GRB,
+            led_indices=[index]
+        )
+        sampling_points.append(new)
+
+    print(f"Sampling points: {len(sampling_points)}")
+    board.set_sampling_points(sampling_points)
