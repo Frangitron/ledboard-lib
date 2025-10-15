50d71f0a64ea69b5712469fe9a0b29b9aedf88a9
MrFrangipane <contact@frangipane.me>
2025-10-15 15:28:33 +0200

    imp(WindowsDrives): return empty values on other platforms

--- ledboardlib/windows_drives.py
+++ ledboardlib/windows_drives.py
@@ -1,13 +1,25 @@
-import pywintypes
-import win32api
+import os
+if os.name != "nt":
+
+    def list_drives() -> list[str]:
+        return []
 
 
-def list_drives() -> list[str]:
-    return win32api.GetLogicalDriveStrings().split('\000')[:-1]
+    def is_drive_pico(drive: str) -> bool:
+        return False
+
+else:
+
+    import pywintypes
+    import win32api
 
 
-def is_drive_pico(drive: str) -> bool:
-    try:
-        return win32api.GetVolumeInformation(drive)[0] == 'RPI-RP2'
-    except pywintypes.error as e:
-        raise OSError(e)
+    def list_drives() -> list[str]:
+        return win32api.GetLogicalDriveStrings().split('\000')[:-1]
+
+
+    def is_drive_pico(drive: str) -> bool:
+        try:
+            return win32api.GetVolumeInformation(drive)[0] == 'RPI-RP2'
+        except pywintypes.error as e:
+            raise OSError(e)
