008d154b14ab516cfc89134f387e5bb50d730ca7
MrFrangipane <contact@frangipane.me>
2025-10-17 12:46:23 +0200

    imp(InteropData): restore dataclass defaults, save Melinerion values to json file

--- ledboardlib/interop_data/dataclass.py
+++ ledboardlib/interop_data/dataclass.py
@@ -10,11 +10,11 @@ from ledboardlib.sampling_point.sampling_point import SamplingPoint
 @dataclass_json
 @dataclass
 class InteropData:
-    artnet_target_ip: str = "127.0.0.1"
+    artnet_target_ip: str | None = None
     default_control_parameters: ControlParameters | None = None
     emulator_ignores_dimmer: bool = True
     enttec_output_enabled: bool = False
-    enttec_port_name: str = "COM18"
+    enttec_port_name: str | None = None
     fixtures: list[Fixture] = field(default_factory=list)
-    midi_port_name: str = "OSC Artnet"
+    midi_port_name: str |None = None
     sampling_points: list[SamplingPoint] = field(default_factory=list)
