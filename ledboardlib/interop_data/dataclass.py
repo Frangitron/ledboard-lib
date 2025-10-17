cde8c37d10ac0e353a66a89c5b798f5c0f808c2e
MrFrangipane <contact@frangipane.me>
2025-10-17 12:32:43 +0200

    dans le train

--- ledboardlib/interop_data/dataclass.py
+++ ledboardlib/interop_data/dataclass.py
@@ -10,6 +10,11 @@ from ledboardlib.sampling_point.sampling_point import SamplingPoint
 @dataclass_json
 @dataclass
 class InteropData:
-    sampling_points: list[SamplingPoint] = field(default_factory=list)
+    artnet_target_ip: str = "127.0.0.1"
     default_control_parameters: ControlParameters | None = None
+    emulator_ignores_dimmer: bool = True
+    enttec_output_enabled: bool = False
+    enttec_port_name: str = "COM18"
     fixtures: list[Fixture] = field(default_factory=list)
+    midi_port_name: str = "OSC Artnet"
+    sampling_points: list[SamplingPoint] = field(default_factory=list)
